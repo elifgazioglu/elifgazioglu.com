77c6f04a7ef769f7985095f61361a7b13d0c164a
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-02-08 23:47:16 +0300

    add images and headers position css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import "./App.css";
 import { SiLinkedin, SiInstagram, SiGithub } from "react-icons/si";
+import image1 from "./images/image1.JPG";
 
 import React, { useState } from "react";
 
 function App() {
   const [state, setState] = useState(false);
-  let url = "https://www.linkedin.com/in/elif-gazioglu";
   return (
     <div className="App">
-      <div class="head-container">
+      <div
+        class="head-container"
+        style={{ position: "fixed", top: 0, left: 0, right: 0, zIndex: 1 }}
+      >
         <div class="logo">
-          <header class="name"> Elif Gazioglu</header>
+          <header class="name">Elif Gazioglu ♡</header>
         </div>
       </div>
       <div class="menu-container">
@@ -19,15 +23,47 @@ function App() {
           <br />
           Gazioglu
         </h1>
-        <p1>Software Developer</p1>
+        <p1 class="larger-paragraph">Software Developer</p1>
         <div class="icons">
           <ul class="list-inline">
-            <li class="social-icons">
-              <a href={url} target="_blank"><SiLinkedin></SiLinkedin></a>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/elif-gazioglu"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SiLinkedin></SiLinkedin>
+              </a>
             </li>
-            <li class="social-icons"><SiInstagram></SiInstagram></li>
-            <li class="social-icons"><SiGithub></SiGithub></li>
+            <li>
+              <a
+                href="https://www.instagram.com/elifgaziogluuu/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SiInstagram></SiInstagram>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/elifgazioglu"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SiGithub></SiGithub>
+              </a>
+            </li>
           </ul>
+        </div>
+      </div>
+      <div class="about-container">
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+        <img
+          src={image1}
+          alt="Image 1"
+          style={{ width: "700px", height: "300px" }}
+        ></img>
+        <h1 class="about-header">About Me</h1>
         </div>
       </div>
     </div>
